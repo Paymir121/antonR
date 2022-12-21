ed4989877457d62dd7e6e4560c91ba7f4e820f30
Романов Никита Вадимович <n.romanov@oncoart.ru>
2022-12-21 14:14:17 +0300

    add users posts static

--- antonR/users/urls.py
+++ antonR/users/urls.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.views import (
     LoginView,
     LogoutView,
-    PasswordChangeDoneView,
-    PasswordChangeView,
-    PasswordResetCompleteView,
-    PasswordResetConfirmView,
-    PasswordResetDoneView,
-    PasswordResetView,
 )
 from django.urls import path
 
@@ -29,47 +23,5 @@ urlpatterns = [
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login',
-    ),
-    path(
-        'password_reset/',
-        PasswordResetView.as_view(
-            template_name='users/password_reset_form.html'
-        ),
-        name='password_reset',
-    ),
-    path(
-        'password_reset/done/',
-        PasswordResetDoneView.as_view(
-            template_name='users/password_reset_done.html'
-        ),
-        name='password_reset_done',
-    ),
-    path(
-        'password_change/',
-        PasswordChangeView.as_view(
-            template_name='users/password_change_form.html'
-        ),
-        name='password_change',
-    ),
-    path(
-        'password_change/done/',
-        PasswordChangeDoneView.as_view(
-            template_name='users/password_change_done.html'
-        ),
-        name='password_change_done',
-    ),
-    path(
-        'reset/done/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html'
-        ),
-        name='password_reset_confirm',
-    ),
-    path(
-        'reset/done/',
-        PasswordResetCompleteView.as_view(
-            template_name='users/password_reset_complete.html'
-        ),
-        name='password_reset_complete',
     ),
 ]
