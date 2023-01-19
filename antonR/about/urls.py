fcab449c8212b6cc43dbca500bb457ecace6f227
paymir121 <nikox12@mail.ru>
2023-01-19 14:55:42 +0300

    add main

--- antonR/about/urls.py
+++ antonR/about/urls.py
@@ -7,6 +7,6 @@ app_name = 'about'
 urlpatterns = [
     path('author/', views.PageAboutAuthor.as_view(), name='author'),
     path('tech/', views.PageTech.as_view(), name='tech'),
-    path('main/', views.MainPage.as_view(), name='main'),
+    path('', views.MainPage.as_view(), name='main'),
     path('company/', views.CompanyPage.as_view(), name='company'),
 ]
--- antonR/posts/urls.py
+++ antonR/posts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('post/', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
